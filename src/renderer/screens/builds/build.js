bc48c2725866094350a1bc4affc1184d7325b3e1
Liam Maricato <liammaricato@gmail.com>
2025-04-05 10:38:24 -0300

    Load build steps

--- src/renderer/screens/builds/build.js
+++ src/renderer/screens/builds/build.js
@@ -1,3 +1,5 @@
+const buildStepsContainer = document.getElementById('build-steps')
+
 const builds = JSON.parse(localStorage.getItem('builds')) || []
 const currentBuildId = sessionStorage.getItem('openBuildId') || 0
 
@@ -5,4 +7,18 @@ const build = builds.find(build => build.id == currentBuildId)
 
 document.getElementById('build-title').textContent = build.title
 
-// Criar os steps da build
+for (step of build.steps) {
+    createBuildStep(step)
+}
+
+function createBuildStep(step) {
+    const stepEl = document.createElement('div')
+    stepEl.classList.add('build-step')
+    stepEl.innerHTML = `
+        <input type="time" name="time" placeholder="Time" value="${step.time}" disabled>
+        <input type="text" name="supply" placeholder="supply" value="${step.supply}" disabled>
+        <input type="text" name="description" placeholder="description" value="${step.description}" disabled>
+    `
+
+    buildStepsContainer.appendChild(stepEl)
+}
