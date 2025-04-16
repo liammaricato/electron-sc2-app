8f24913d3bd5ad41200d2a69cb3bdddd5de709e7
Liam Maricato <liammaricato@gmail.com>
2025-04-15 23:00:04 -0300

    Start player

---
+++ src/renderer/screens/builds/player/player.js
@@ -0,0 +1,28 @@
+const buildStepsContainer = document.getElementById('build-steps')
+
+const builds = JSON.parse(localStorage.getItem('builds')) || []
+const currentBuildId = sessionStorage.getItem('openBuildId') || 0
+
+const build = builds.find(build => build.id == currentBuildId)
+
+if (!build) {
+    // close()
+}
+
+document.getElementById('build-title').textContent = build.title || 'Untitled build'
+
+for (step of build.steps) {
+    createBuildStep(step)
+}
+
+function createBuildStep(step) {
+    const stepEl = document.createElement('div')
+    stepEl.classList.add('build-step')
+    stepEl.innerHTML = `
+        <input type="time" name="time" placeholder="Time" value="${step.time}" disabled>
+        <input type="text" name="supply" placeholder="supply" value="${step.supply}" disabled>
+        <input type="text" name="description" placeholder="description" value="${step.description}" disabled>
+    `
+
+    buildStepsContainer.appendChild(stepEl)
+}
